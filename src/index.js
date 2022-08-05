049bf87f8e67fce6eaf365813d0c59b805359b61
aashrun <aashrungautam15@gmail.com>
2022-08-05 22:06:55 +0530

    updated

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const mongoose = require("mongoose");
+const app = express();
+const multer =require("multer");
+
+app.use(bodyParser.json());
+app.use(multer().any());
+
+mongoose.connect(
+    "mongodb+srv://shivani-0515:shivani0515@shivani.o6uy2fw.mongodb.net/shivani0507-DB?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected."))
+  .catch( err => console.log(err));
+
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
---
+++ src/middlewares/auth.js
@@ -0,0 +1,2 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose")
