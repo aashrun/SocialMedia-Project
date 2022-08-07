af3f366fd702dfb170e062def859b49b00512ec4
aashrun <aashrungautam15@gmail.com>
2022-08-07 17:42:23 +0530

    h

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,11 +1,13 @@
 const profileModel = require("../models/profileModel.js")
+const validation = require("../validations/validator.js")
 
 
 
 const createProfile = async function (req, res){
     try{
         let body = req.body
-        
+        let {fullName, userName, DOB, email, password, mobileNo, profileImage, bio, followerCount, followingCount, followerList, followingList, postCount, postData, location, blockedAccs} = body
+
     }catch(error){
         res.status(500).send({status : false, message : error.message})
     }
---
+++ src/validations/validator.js
@@ -0,0 +1,21 @@
+const validator = require("email-validator");
+
+
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    return true;
+};
+
+const emptyBody = function (data) {
+    return Object.keys(data).length > 0;
+};
+
+const emailCheck = function (value) {
+    if (validator.validate(value.trim())) {
+        return true;
+    }
+    return false;
+};
+
+module.exports = {isValid, emptyBody, emailCheck}
