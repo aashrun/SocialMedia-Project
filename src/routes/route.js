3bb18755ec229d703ddaa01502b6a53e07878821
aashrun <aashrungautam15@gmail.com>
2022-09-06 16:24:23 +0530

    updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -19,25 +19,26 @@ router.put("/profile/:profileId/update",  profileController.updateProfile)
 
 router.delete("/profile/:profileId/delete", MW.authentication, MW.authorization, profileController.deleteProfile)
 
-router.put("/profile/:profileId/follow", profileController.followProfile)
+router.put("/profile/:profileId/follow", MW.authentication, MW.authorization, profileController.followProfile)
 
-router.put("/profile/:profileId/unfollow", profileController.unfollowProfile)
+router.put("/profile/:profileId/unfollow", MW.authentication, MW.authorization, profileController.unfollowProfile)
 
-router.put("/profile/:profileId/block",  profileController.blockProfile)
+router.put("/profile/:profileId/block", MW.authentication, MW.authorization, profileController.blockProfile)
 
 router.put("/profile/:profileId/unblock", MW.authentication, MW.authorization, profileController.unblockProfile)
 
-router.put("/profile/:profileId/comment",  profileController.commentOnPost)
+router.put("/profile/:profileId/comment", MW.authentication, MW.authorization,  profileController.commentOnPost)
 
-router.delete("/profile/:profileId/deleteComment", profileController.deleteComment)
+router.delete("/profile/:profileId/deleteComment", MW.authentication, MW.authorization, profileController.deleteComment)
 
 router.put("/profile/:profileId/like", MW.authentication, MW.authorization, profileController.likePost)
 
 router.put("/profile/:profileId/unlike", MW.authentication, MW.authorization, profileController.unlikePost)
 
-router.get("/profile/:profileId/followerOrFollowingList", profileController.followerOrFollowingList)
+router.get("/profile/:profileId/followerOrFollowingList", MW.authentication, MW.authorization, profileController.followerOrFollowingList)
 
-router.get("/profile/:profileId/getBlockedAccount", profileController.getBlockedAccount)
+router.get("/profile/:profileId/getBlockedAccount", MW.authentication, MW.authorization, profileController.getBlockedAccount)
+
 
 
 
@@ -53,6 +54,9 @@ router.get("/post/:profileId/getCommentsList/:postId", MW.authentication, MW.aut
 router.put("/post/:profileId/updatePost/:postId", MW.authentication, MW.authorization, postController.updatePost)
 
 router.delete("/post/:profileId/deletePost/:postId", MW.authentication, MW.authorization, postController.deletePost)
+
+
+
 
 
 //====================================  Invalid API  ==========================================//
